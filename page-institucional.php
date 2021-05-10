89b7eb5f0ce559eb67ae13591e289f8302075c05
Erick <santoserick9@gmail.com>
2021-05-09 21:52:36 -0300

    criada a estrutura e estilo inicial da sessão de localização"

--- page-institucional.php
+++ page-institucional.php
@@ -1,9 +1,27 @@
 <?php 
   //Template Name: Institucional
   get_header();
-
-  get_template_part('components/headerPage');
 ?>
+
+<main class="pageInstitucional">
+  <?php get_template_part('components/headerPage'); ?>
+
+  <section class="localizacao">
+    <div class="container">
+      <div class="row">
+        <div class="content text-right">
+          <p>Com nossa matriz localizada em Recife ( PE ), estamos presentes 
+          em mais de 800 municípios através de nossas operações e filiais em 
+          diversas Capitais do Brasil.</p>
+          <p>Somos ágeis e flexíveis para estarmos onde está o nosso Cliente.</p>
+        </div>
+        <div class="title">
+          <h3>LOCALIZAÇÃO</h3>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
 
 <h1>institucional</h1>
 
