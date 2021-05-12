ca821ccdf7a22a12e35a2aa33e2c22e5085cc9b6
Erick <santoserick9@gmail.com>
2021-05-11 22:29:56 -0300

    finalizada a a estrutua da pagina de clientes e estilo da pagina desktop / mobile

--- page-clientes.php
+++ page-clientes.php
@@ -4,7 +4,122 @@
 ?>
 
 <main class="pageClients">
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto id magnam sapiente eum, vel nobis! Ea explicabo aspernatur recusandae numquam.</p>
+  <?php get_template_part('components/headerPage'); ?>
+
+  <section class="listClients">
+    <div class="container">
+      <ul>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+        <li>
+          <img src="<?php bloginfo('template_url'); ?>/clientes/1.png" alt="1">
+        </li>
+      </ul>
+    </div>
+  </section>
 </main>
 
 <?php get_footer(); ?>
