98500bc7d98a4d5b6981604d5c07d43f1dde0480
unknown <santoserick@gmail.com>
2021-05-18 22:05:13 -0300

    formulário substituido por contact form

--- page-contato.php
+++ page-contato.php
@@ -10,45 +10,7 @@
     <div class="content">
       <div class="form">
         <h3>Fale conosco</h3>
-        <form action="">
-          <div class="formGroup">
-            <label for="nome">Seu Nome</label>
-            <input
-              type="text"
-              name="nome"
-              id="nome"
-              placeholder="Seu Nome"
-            />
-          </div>
-          <div class="formGroup">
-            <label for="telefone">Seu Telefone</label>
-            <input
-              type="text"
-              name="telefone"
-              id="telefone"
-              placeholder="Seu Telefone"
-            />
-          </div>
-          <div class="formGroup">
-            <label for="assunto">Assunto</label>
-            <input
-              type="text"
-              name="assunto"
-              id="assunto"
-              placeholder="Assunto"
-            />
-          </div>
-          <div class="formGroup">
-            <label for="mensagem">Sua Mensagem</label>
-            <textarea 
-              name="mensagem" 
-              id="mensagem" 
-              rows="4" 
-              placeholder="Sua Mensagem"
-            ></textarea>
-          </div>
-          <button type="submit">ENVIAR</button>
-        </form>
+        <?= do_shortcode('[contact-form-7 id="196" title="Formulário de contato - Página Contato"]'); ?>
       </div>
     </div>
   </section>
